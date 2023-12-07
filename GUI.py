530513d8cde79a70eb994183200f0aa9bae25008
Meawz <maftei.cristian23@gmail.com>
2023-12-07 14:10:32 +0200

    Some changes

--- GUI.py
+++ GUI.py
@@ -1,19 +1,62 @@
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QMessageBox, QLineEdit, QVBoxLayout
 from PyQt5.QtGui import QFont
 import PyPDF2
 
+class NewWindow(QWidget):
+    def __init__(self, parent):
+        super().__init__()
 
+        self.parent = parent
+
+        self.setWindowTitle("New Window")
+        self.setGeometry(300, 300, 315, 250)
+
+        self.setStyleSheet("QWidget { background-color: black; border: 2px solid black; border-radius: 8px; }"
+                           "QPushButton { background-color: #8F00FF; color: white; border: 2px solid #6F2DA8; padding: 10px; font-size: 14px; border-radius: 5px; }"
+                           "QPushButton:hover { background-color: #6F2DA8; }")
+
+        self.name_input = QLineEdit(self)
+        self.name_input.setPlaceholderText("Enter your name")
+        self.name_input.setStyleSheet("background-color: #FFFFCC; border: 2px solid #FF9900; padding: 5px;")
+
+        self.email_input = QLineEdit(self)
+        self.email_input.setPlaceholderText("Enter your email")
+        self.email_input.setStyleSheet("background-color: #FFFFCC; border: 2px solid #FF9900; padding: 5px;")
+
+        back_button = QPushButton("Back", self)
+        back_button.clicked.connect(self.go_back)
+        back_button.setGeometry(30, 70, 120, 50)
+
+        exit_button = QPushButton("Exit", self)
+        exit_button.clicked.connect(self.close)
+        exit_button.setGeometry(160, 70, 120, 50)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.name_input)
+        layout.addWidget(self.email_input)
+        layout.addWidget(back_button)
+        layout.addWidget(exit_button)
+
+        self.setLayout(layout)
+        
+
+    def go_back(self):
+        self.close()
+        self.parent.show()
 
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.new_window = None
+
         self.setWindowTitle("Python GUI Test")
         self.setGeometry(150, 150, 315, 250)
 
-        self.setStyleSheet("QWidget { background-color: black; }"
-                           "QPushButton { background-color: #8F00FF; color: white; border: none; padding: 10px; font-size: 14px; border-radius: 5px; }"
+
+        self.setStyleSheet("QWidget { background-color: black; border: 2px solid black; border-radius: 8px; }"
+                           "QPushButton { background-color: #8F00FF; color: white; border: 2px solid #6F2DA8; padding: 10px; font-size: 14px; border-radius: 5px; }"
                            "QPushButton:hover { background-color: #6F2DA8; }") 
 
         self.init_ui()
@@ -26,6 +69,10 @@ class MyWindow(QWidget):
         exit_button = QPushButton("Exit", self)
         exit_button.clicked.connect(self.close)
         exit_button.setGeometry(160, 70, 120, 50)
+
+        new_window_button = QPushButton("Open New Window", self)
+        new_window_button.clicked.connect(self.open_new_window)
+        new_window_button.setGeometry(30, 130, 250, 50)
 
         font = QFont()
         font.setPointSize(12)
@@ -62,6 +109,11 @@ class MyWindow(QWidget):
                QMessageBox.warning(self, "Error", "Invalid output file name.")
         else:
             QMessageBox.warning(self, "No PDFs", "No PDF files selected.")
+    
+    def open_new_window(self):
+        self.new_window = NewWindow(self)
+        self.new_window.show()
+        self.close()
 
 if __name__ == "__main__":
     app = QApplication([])
